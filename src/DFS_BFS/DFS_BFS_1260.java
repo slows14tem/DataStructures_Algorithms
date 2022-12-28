700c18d40c7da28a6817646f0636ee25695569fc
slows14tem <slows14@naver.com>
2022-12-28 22:57:54 +0900

    2022-12-28

--- src/DFS_BFS/DFS_BFS_1260.java
+++ src/DFS_BFS/DFS_BFS_1260.java
@@ -3,40 +3,69 @@ package DFS_BFS;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class DFS_BFS_1260 {
 
 
-  public static void main(String[] args) throws IOException{
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    String str = br.readLine();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
 		StringTokenizer st = new StringTokenizer(str);
 
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-    int V = Integer.parseInt(st.nextToken());
-    int[][] graph = new int[N+1][N+1];
+		int N = Integer.parseInt(st.nextToken());	//node
+		int K = Integer.parseInt(st.nextToken());	//link
+		int V = Integer.parseInt(st.nextToken());	//first search
+		int[][] graph = new int[N+1][N+1];
 
-    for(int i=0;i<K;i++){
-      String links = br.readLine();
-      StringTokenizer stl = new StringTokenizer(links);
-      int node1 = Integer.parseInt(stl.nextToken());
-      int node2 = Integer.parseInt(stl.nextToken());
-      graph[node1][node2]=1;
-    }
+		for(int i=0;i<K;i++){
+			//그래프를 행렬로 변경
+			String links = br.readLine();
+			StringTokenizer stl = new StringTokenizer(links);
+			int node1 = Integer.parseInt(stl.nextToken());
+			int node2 = Integer.parseInt(stl.nextToken());
+			graph[node1][node2]=1;
+			graph[node2][node1]=1;
+		}
 
-    boolean[] visited = new boolean[N+1];
+		boolean[] visited = new boolean[N+1];
+		DFS(graph, V, visited);
+		System.out.println();
+		visited = new boolean[N+1];
+		BFS(graph, V, visited);
 
-    DFS(graph, V, visited);
+	}
 
-  }
-
-  public void DFS(int[][] graph, int V, boolean[] visited){
-    visited[V] = true;
-    for (int i=0;i<)
-
-
-  }
-  
+	public static void DFS(int[][] graph, int V, boolean[] visited){
+		visited[V] = true;
+		System.out.print(V + " ");
+		for (int i=1;i<graph[V].length;i++) {
+			if (graph[V][i] == 1 && !visited[i]) {
+				DFS(graph, i, visited);
+			}
+		}
+	}
+	
+	public static void BFS(int[][] graph, int V, boolean[] visited) {
+		System.out.print(V + " ");
+		Queue<Integer> q = new LinkedList<>();
+		q.add(V);
+		visited[V]=true;
+		while(true) {
+			for (int i=1;i<graph[V].length;i++) {
+				if(graph[V][i] == 1 && !visited[i]) {
+					q.add(i);
+					visited[i]=true;
+					System.out.print(i + " ");
+				}
+			}
+			q.remove(V);
+			
+			int size = q.size();
+			if(size == 0) break;
+			V = q.peek();
+		}
+	}
 }
---
+++ src/DFS_BFS/DFS_BFS_2178.java
@@ -0,0 +1,89 @@
+package DFS_BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class DFS_BFS_2178 {
+	static boolean visited[][];	//방문체크
+	static int maze[][];	//미로(0:벽)
+	static int mx[] = {0, 0, -1, 1};
+	static int my[] = {-1, 1, 0, 0};	//상 하 좌 우
+	static Queue<Node> que = new LinkedList<>();
+	
+	static int nowx, nowy;	//현재위치 좌표
+	static int N, M;	//가로 세로
+	static int x, y;	//node좌표 입력값
+	
+	static class Node {
+		int x;
+		int y;
+
+		public Node(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken()); // 세로
+		M = Integer.parseInt(st.nextToken()); // 가로
+
+		visited = new boolean[N][M];
+		maze = new int[N][M];
+		
+		for (int i=0;i<N;i++) {
+			String[] str = br.readLine().split("");
+			for (int j=0;j<M;j++) {
+				maze[i][j] = Integer.parseInt(str[j]);
+			}
+		}
+		
+		BFS(0, 0);
+		System.out.println(maze[N-1][M-1]);
+		
+	}
+	
+	public static void BFS(int x, int y) {
+		que.offer(new Node(x, y));
+		//최초 0,0 큐에 입력
+		
+		visited[x][y] = true;
+		//0,0 방문 체크
+		
+		while(!que.isEmpty()) {
+			Node node = que.poll();
+			//큐의 처음 값을 node로 반환하고 큐에서 삭제
+			
+			for (int i=0;i<4;i++) {
+				nowx = node.x + mx[i];
+				nowy = node.y + my[i];
+				//좌표를 상하좌우로 움직임
+				
+				if (Maze_Check() == true && maze[nowx][nowy] != 0 && visited[nowx][nowy] == false) {
+					que.offer(new Node(nowx, nowy));
+					visited[nowx][nowy] = true;
+					maze[nowx][nowy] = maze[node.x][node.y] + 1;
+					//이동할 수 있는 칸이면 그 칸을 큐에 입력, 방문 체크, 이동 전 node좌표값에서 1을 추가(지나온 최소 칸수)
+				}
+			}
+		}	
+	}
+	
+	public static boolean Maze_Check() {
+		//현재 위치가 미로 칸 안쪽에 위치하는지 체크
+		if(nowx >= 0 && nowx < N && nowy >= 0 && nowy < M) return true;
+		else return false;
+	}
+}
+
+//DFS알고리즘 특성상 최단거리를 찾으려면 완전 탐색을 하고 그중에서 가장 작은 값을 선택해야 하는데 경로가 아주 많을 수 있으므로 시간 복잡도가 매우 커진다. 
+//반면 BFS는 최단거리를 보장하기 때문에 이러한 문제들(최단거리 구하기)은 BFS로 풀어야 한다.
+
+//https://coding-factory.tistory.com/602 (큐 관련)
